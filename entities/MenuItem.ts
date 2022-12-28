760bb963a05af51a08dcba6f0d94e17cc9b57e22
Grapeka <chotwilaiwan@gmail.com>
2022-12-28 14:02:17 +0700

    feat: create entities

---
+++ entities/MenuItem.ts
@@ -0,0 +1,45 @@
+export class MenuItem {
+  private id: string;
+  private name: string;
+  private description: string;
+  private price: number;
+
+  constructor(id: string, name: string, description: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getDescription(): string {
+    return this.description;
+  }
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  setId(id: string): void {
+    this.id = id;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  setDescription(description: string): void {
+    this.description = description;
+  }
+
+  setPrice(price: number): void {
+    this.price = price;
+  }
+}
---
+++ entities/Merchant.ts
@@ -0,0 +1,72 @@
+export class Merchant {
+  private id: string;
+  private name: string;
+  private email: string;
+  private password: string;
+  private facebook: string;
+  private instagram: string;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    facebook: string,
+    instagram: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.facebook = facebook;
+    this.instagram = instagram;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+
+  getFacebook(): string {
+    return this.facebook;
+  }
+
+  getInstagram(): string {
+    return this.instagram;
+  }
+
+  setId(id: string): void {
+    this.id = id;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  setEmail(email: string): void {
+    this.email = email;
+  }
+
+  setPassword(password: string): void {
+    this.password = password;
+  }
+
+  setFacebook(facebook: string): void {
+    this.facebook = facebook;
+  }
+
+  setInstagram(instagram: string): void {
+    this.instagram = instagram;
+  }
+}
