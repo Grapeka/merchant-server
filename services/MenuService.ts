e963e8d7abce40263593c27df603468674e2ba7c
Grapeka <chotwilaiwan@gmail.com>
2022-12-28 14:05:09 +0700

    feat: create menu service

---
+++ services/MenuService.ts
@@ -0,0 +1,30 @@
+import { IMenuRepository } from '../interfaces/IMenuRepository';
+import { IMenuItem } from '../interfaces/IMenuItem';
+
+export class MenuService {
+  private menuRepository: IMenuRepository;
+
+  constructor(menuRepository: IMenuRepository) {
+    this.menuRepository = menuRepository;
+  }
+
+  async getAllMenuItems(): Promise<IMenuItem[] | null> {
+    return this.menuRepository.getAllMenuItems();
+  }
+
+  async getMenuItemById(id: number): Promise<IMenuItem | null> {
+    return this.menuRepository.getMenuItemById(id);
+  }
+
+  async getMenuItemsByOwnerId(ownerId: number): Promise<IMenuItem[] | null> {
+    return this.menuRepository.getMenuItemsByOwnerId(ownerId);
+  }
+
+  async saveMenuItem(menuItem: IMenuItem): Promise<void> {
+    return this.menuRepository.saveMenuItem(menuItem);
+  }
+
+  async removeMenuItem(id: number): Promise<void> {
+    return this.menuRepository.removeMenuItem(id);
+  }
+}
