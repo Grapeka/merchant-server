6a414e6e99e4b5de750e33aecb480e886d22e45d
Grapeka <chotwilaiwan@gmail.com>
2022-12-28 14:01:37 +0700

    feat: connect server to mongoDB

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+import mongoose from 'mongoose';
 import authRoute from './routes/authRoute';
 import menuRoute from './routes/menuRoute';
 
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI ?? '', {}, () => {
+  console.log('Connected to DB');
+});
 
 app.use('/auth', authRoute);
 app.use('/menu', menuRoute);
