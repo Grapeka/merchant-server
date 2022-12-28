97fa8f8db1894c0aaf8769b55036a55d1c1c7b64
Grapeka <chotwilaiwan@gmail.com>
2022-12-28 14:02:46 +0700

    feat: create model classes

---
+++ models/MenuItemModel.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { IMenuItem } from '../interfaces/IMenuItem';
+
+export const menuItemSchema: mongoose.Schema<IMenuItem> = new mongoose.Schema({
+  ownerId: { type: Number, required: true },
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+});
+
+export class MenuItemModel {
+  private static instance: MenuItemModel;
+  public model: mongoose.Model<IMenuItem>;
+
+  private constructor() {
+    this.model = mongoose.model('MenuItem', menuItemSchema);
+  }
+
+  public static getInstance(): MenuItemModel {
+    if (!MenuItemModel.instance) {
+      MenuItemModel.instance = new MenuItemModel();
+    }
+    return MenuItemModel.instance;
+  }
+}
---
+++ models/MerchantModel.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { IMerchant } from '../interfaces/IMerchant';
+
+export const merchantSchema: mongoose.Schema<IMerchant> = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  facebook: { type: String, required: true },
+  instagram: { type: String, required: true },
+});
+
+export class MerchantModel {
+  private static instance: MerchantModel;
+  public model: mongoose.Model<IMerchant>;
+
+  private constructor() {
+    this.model = mongoose.model('Merchant', merchantSchema);
+  }
+
+  public static getInstance(): MerchantModel {
+    if (!MerchantModel.instance) {
+      MerchantModel.instance = new MerchantModel();
+    }
+
+    return MerchantModel.instance;
+  }
+}
