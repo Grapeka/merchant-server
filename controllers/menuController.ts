a7d6e6607bd75a8e6977d960668e311feac6feef
Grapeka <chotwilaiwan@gmail.com>
2023-01-07 15:35:38 +0700

    refactor: convert menu func controller to class

--- controllers/menuController.ts
+++ controllers/menuController.ts
@@ -8,47 +8,94 @@ const menuItemModel = MenuItemModel.getInstance();
 const mongoMenuRepository = new MongoMenuRepository(menuItemModel);
 const mongoMenuService = new MenuService(mongoMenuRepository);
 
-export const getAllMenuItems = async (
-  req: Request,
-  res: Response
-): Promise<Response<IMenuItem[]>> => {
-  const menuItems = await mongoMenuService.getAllMenuItems();
-  return res.json(menuItems);
-};
-
-export const getMenuItemById = async (
-  req: Request,
-  res: Response
-): Promise<Response<IMenuItem>> => {
-  const { id } = req.params;
-
-  const menuItem = await mongoMenuService.getMenuItemById(parseInt(id));
-
-  if (menuItem === undefined || menuItem === null) {
-    return res.sendStatus(404);
+export class MenuController {
+  async getAllMenuItems(
+    req: Request,
+    res: Response
+  ): Promise<Response<IMenuItem[]>> {
+    const menuItems = await mongoMenuService.getAllMenuItems();
+    return res.json(menuItems);
   }
 
-  return res.json(menuItem);
-};
+  async getMenuItemById(
+    req: Request,
+    res: Response
+  ): Promise<Response<IMenuItem>> {
+    const { id } = req.params;
 
-export const getMenuItemsByMerchantId = async (
-  req: Request | any,
-  res: Response
-): Promise<Response<IMenuItem | IMenuItem[] | any>> => {
-  const merchantId = req.user.id;
+    const menuItem = await mongoMenuService.getMenuItemById(parseInt(id));
 
-  if (req.user.id !== parseInt(merchantId)) {
-    return res.sendStatus(403);
+    if (menuItem === undefined || menuItem === null) {
+      return res.sendStatus(404);
+    }
+
+    return res.json(menuItem);
   }
 
-  const menuItems = await mongoMenuService.getMenuItemsByOwnerId(merchantId);
+  async getMenuItemsByMerchantId(
+    req: Request | any,
+    res: Response
+  ): Promise<Response<IMenuItem | IMenuItem[] | any>> {
+    const merchantId = req.user.id;
 
-  if (menuItems === null || menuItems === undefined) {
-    return res.sendStatus(404);
-  }
+    if (req.user.id !== parseInt(merchantId)) {
+      return res.sendStatus(403);
+    }
 
-  if (menuItems.length) {
-    return res.sendStatus(404);
+    const menuItems = await mongoMenuService.getMenuItemsByOwnerId(merchantId);
+
+    if (menuItems === null || menuItems === undefined) {
+      return res.sendStatus(404);
+    }
+
+    if (menuItems.length) {
+      return res.sendStatus(404);
+    }
+    return res.json(menuItems);
   }
-  return res.json(menuItems);
-};
+}
+
+// export const getAllMenuItems = async (
+//   req: Request,
+//   res: Response
+// ): Promise<Response<IMenuItem[]>> => {
+//   const menuItems = await mongoMenuService.getAllMenuItems();
+//   return res.json(menuItems);
+// };
+
+// export const getMenuItemById = async (
+//   req: Request,
+//   res: Response
+// ): Promise<Response<IMenuItem>> => {
+//   const { id } = req.params;
+
+//   const menuItem = await mongoMenuService.getMenuItemById(parseInt(id));
+
+//   if (menuItem === undefined || menuItem === null) {
+//     return res.sendStatus(404);
+//   }
+
+//   return res.json(menuItem);
+// };
+
+// export const getMenuItemsByMerchantId = async (
+//   req: Request | any,
+//   res: Response
+// ): Promise<Response<IMenuItem | IMenuItem[] | any>> => {
+//   const merchantId = req.user.id;
+
+//   if (req.user.id !== parseInt(merchantId)) {
+//     return res.sendStatus(403);
+//   }
+
+//   const menuItems = await mongoMenuService.getMenuItemsByOwnerId(merchantId);
+
+//   if (menuItems === null || menuItems === undefined) {
+//     return res.sendStatus(404);
+//   }
+
+//   if (menuItems.length) {
+//     return res.sendStatus(404);
+//   }
+//   return res.json(menuItems);
+// };
