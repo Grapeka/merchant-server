c4b2c72ab8cc3fef71b53996c047e76c62780a3c
Grapeka <chotwilaiwan@gmail.com>
2023-01-07 15:57:23 +0700

    feat: use create menu from menu service

--- controllers/menuController.ts
+++ controllers/menuController.ts
@@ -3,12 +3,42 @@ import { MongoMenuRepository } from '../repositories/mongo/MongoMenuRepository';
 import { MenuService } from '../services/MenuService';
 import { MenuItemModel } from '../models/MenuItemModel';
 import { Request, Response } from 'express';
+import crypto from 'crypto';
 
 const menuItemModel = MenuItemModel.getInstance();
 const mongoMenuRepository = new MongoMenuRepository(menuItemModel);
 const mongoMenuService = new MenuService(mongoMenuRepository);
 
 export class MenuController {
+  createMenuItem(req: Request | any, res: Response): Response<IMenuItem> {
+    const merchantId = req.user.id;
+
+    const { name, description, price, ownerId, category, image } = req.body;
+
+    if (merchantId !== ownerId) {
+      return res.sendStatus(403);
+    }
+    const id = crypto.randomUUID();
+
+    if (
+      id === undefined ||
+      name === undefined ||
+      description === undefined ||
+      price === undefined ||
+      ownerId === undefined ||
+      category === undefined ||
+      image === undefined ||
+      image === ''
+    ) {
+      return res.sendStatus(400);
+    }
+
+    const menuItem = { id, name, description, price, ownerId, category, image };
+
+    mongoMenuService.createMenuItem(menuItem as IMenuItem);
+
+    return res.json(menuItem);
+  }
   async getAllMenuItems(
     req: Request,
     res: Response
