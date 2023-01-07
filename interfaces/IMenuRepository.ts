b13bbe9e4775dad9af206f11535962a418e15110
Grapeka <chotwilaiwan@gmail.com>
2023-01-07 15:56:26 +0700

    feat: add create menu to menu repo interface

--- interfaces/IMenuRepository.ts
+++ interfaces/IMenuRepository.ts
@@ -1,6 +1,7 @@
 import { IMenuItem } from './IMenuItem';
 
 export interface IMenuRepository {
+  createMenuItem(menuItem: IMenuItem): Promise<void>;
   getAllMenuItems(): Promise<IMenuItem[] | null>;
   getMenuItemById(id: number): Promise<IMenuItem | null>;
   getMenuItemsByOwnerId(ownerId: number): Promise<IMenuItem[] | null>;
